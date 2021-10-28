8ac88eb20d034ab545195cd725f6489d78c009de
Tonlisboa <tonlisboa@hotmail.com>
2021-10-28 10:52:41 -0300

    Inicio fim

--- app/src/main/java/com/example/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.alertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void abrirAlertDialog(View view){
+        //Instanciar AlettDialog
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        //Configura título
+        dialog.setTitle("Título da Dialog");
+        dialog.setMessage("Mensagem da Dialog");
+        //Configurar Cancelamento
+        dialog.setCancelable(false);
+        //Configurar icone
+        dialog.setIcon(android.R.drawable.ic_btn_speak_now);
+        //Configurar ações para sim e não
+        dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Executar ação ao clicar no botão Sim",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        dialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Executar ação ao clicar no botão Não",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        //Criar e exibir AlertDialog
+        dialog.create();
+        dialog.show();
     }
 }
